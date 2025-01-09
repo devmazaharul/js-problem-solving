373df7ec88ad36e95a6809657180b9f1b74335bc
mazaharul islam <expertmazaharul@gmail.com>
2025-01-09 17:24:12 +0600

    js problems

---
+++ index.js
@@ -0,0 +1,61 @@
+// Compact Object
+/**
+ * @param {Object|Array}
+ * @return {Object|Array}
+ */
+var compactObject = function (obj) {
+  let resultobj = {};
+  for (let i in obj) {
+    if (Array.isArray(obj[i])) {
+      const filRes = obj[i].filter((v) => {
+        if (v !== false || v !== null || v !== 0) {
+          return v;
+        }
+      });
+
+      resultobj[i] = filRes;
+    } else {
+      if (obj[i] !== false && obj[i] !== null && obj[i] !== 0) {
+        resultobj[i] = obj[i];
+      }
+    }
+  }
+  // console.log(resultobj)
+  return resultobj;
+};
+
+const object = {
+  a: 0,
+  b: [4, null, false, 1],
+  c: [null, 58],
+  ss: null,
+};
+
+compactObject(object);
+
+
+
+
+//find vowel form string
+
+
+
+const findVowel=(str="")=>{
+  const result={
+    count:0,
+    chart:[]
+  }
+  const vowel=["a","e","i","o","u"];
+
+  for (let i = 0; i < str.length; i++) {
+    if(vowel.includes(str[i])){
+      result.chart=[...result.chart,str[i]]
+      result.count++
+    }
+
+  }
+  return result;
+}
+
+findVowel("hellow world")
+
